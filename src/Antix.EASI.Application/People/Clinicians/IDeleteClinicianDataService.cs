1222e97aafe3778d3161aec26ac85b78853ca0df
Anthony Johnston <aj@antix.co.uk>
2015-01-17 17:12:21 +0000

    missing file

---
+++ src/Antix.EASI.Application/People/Clinicians/IDeleteClinicianDataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Antix.Services;
+
+namespace Antix.EASI.Application.People.Clinicians
+{
+    public interface IDeleteClinicianDataService :
+        IServiceIn<Guid>
+    {
+    }
+}
