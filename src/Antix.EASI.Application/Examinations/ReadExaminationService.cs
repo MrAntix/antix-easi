681bf47d85d5f1a33faee3961f552c53204395ad
Anthony Johnston <aj@antix.co.uk>
2015-02-02 08:32:11 +0000

    missing service

---
+++ src/Antix.EASI.Application/Examinations/ReadExaminationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Antix.EASI.Domain.Examinations;
+using Antix.EASI.Domain.Examinations.Models;
+using Antix.Services.Models;
+
+namespace Antix.EASI.Application.Examinations
+{
+    public class ReadExaminationService :
+        IReadExaminationService
+    {
+        readonly IReadExaminationDataService _dataService;
+
+        public ReadExaminationService(IReadExaminationDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+        public async Task<IServiceResponse<ExaminationModel>> ExecuteAsync(Guid model)
+        {
+            var result = await _dataService.ExecuteAsync(model);
+
+            return ServiceResponse.Empty.WithData(result);
+        }
+    }
+}
