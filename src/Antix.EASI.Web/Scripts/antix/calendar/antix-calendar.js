cbc0e0290240cb20fd230e7b99fe7bf509d55584
Anthony Johnston <aj@antix.co.uk>
2015-02-12 16:36:49 +0000

    not using raf any more - doesn't make any difference

--- src/Antix.EASI.Web/Scripts/antix/calendar/antix-calendar.js
+++ src/Antix.EASI.Web/Scripts/antix/calendar/antix-calendar.js
@@ -6,11 +6,9 @@ angular.module('antix.calendar', [
     .directive('antixCalendar',
     [
         '$log', '$timeout', '$document',
-        '$$rAF',
         'AntixCalendarService',
         function (
             $log, $timeout, $document,
-            $$rAF,
             CalendarService) {
 
             $log.debug('antixCalendar.init');
@@ -25,114 +23,94 @@ angular.module('antix.calendar', [
                     var containerDom = $document.prop('body'),
                         calendar = CalendarService.createCalendar(element, new Date()),
                         calendarInitialPadding = 2000,
-                        calendarTopPadding = 0,
-                        calendarBottomPadding = 0,
                         cellHeight = 40,
                         monthOffset = -36,
                         getMonthHeight = function (weeks) { return weeks * cellHeight + monthOffset; };
 
                     scope.months = calendar.months;
 
-                    var load = function () {
+                    var reset = function () {
+                        $log.debug('antixCalendar.reset()');
 
-                        scope.$apply(function () {
+                        var watchers = scope.$$watchers;
+                        scope.$$watchers = [];
 
-                            var watchers = scope.$$watchers;
-                            scope.$$watchers = [];
+                        var calendarTopPadding = calendarInitialPadding,
+                            calendarBottomPadding = calendarInitialPadding,
+                            scrollTop = containerDom.scrollTop,
+                            containerHeight = window.innerHeight,
+                            elementHeight = element[0].offsetHeight - calendarTopPadding;
 
-                            var scrollTop = containerDom.scrollTop,
-                                containerHeight = window.innerHeight,
-                                elementHeight = element[0].offsetHeight - calendarTopPadding;
+                        while (scrollTop - 300 < calendarTopPadding) {
+                            $log.debug('antixCalendar.min.add()');
 
-                            while (scrollTop - 300 < calendarTopPadding) {
-                                $log.debug('antixCalendar.min.add()');
+                            calendar.min.add();
 
-                                calendar.min.add();
+                            calendarTopPadding -= getMonthHeight(calendar.min.weeks);
+                        }
 
-                                calendarTopPadding -= getMonthHeight(calendar.min.weeks);
-                            }
+                        var offset = elementHeight
+                            + calendarTopPadding
+                            - scrollTop;
 
-                            var offset = elementHeight
-                                + calendarTopPadding
-                                - scrollTop;
+                        while (offset
+                            - calendarBottomPadding
+                            - 300
+                            < containerHeight) {
+                            $log.debug('antixCalendar.max.add()');
 
-                            while (offset
-                                - calendarBottomPadding
-                                - 300
-                                < containerHeight) {
-                                $log.debug('antixCalendar.max.add()');
+                            calendar.max.add();
 
-                                calendar.max.add();
+                            calendarBottomPadding -=
+                                getMonthHeight(calendar.max.weeks);
+                        }
 
-                                calendarBottomPadding -=
-                                    getMonthHeight(calendar.max.weeks);
-                            }
+                        while (scrollTop - 600 > calendarTopPadding) {
+                            $log.debug('antixCalendar.min.remove()');
 
-                            while (scrollTop - 600 > calendarTopPadding) {
-                                $log.debug('antixCalendar.min.remove()');
+                            calendarTopPadding += getMonthHeight(calendar.min.weeks);
 
-                                calendarTopPadding += getMonthHeight(calendar.min.weeks);
+                            calendar.min.remove();
+                        }
 
-                                calendar.min.remove();
-                            }
+                        while (offset
+                            - calendarBottomPadding
+                            - 600
+                            > containerHeight) {
+                            $log.debug('antixCalendar.max.remove()');
 
-                            while (offset
-                                - calendarBottomPadding
-                                - 600
-                                > containerHeight) {
-                                $log.debug('antixCalendar.max.remove()');
+                            calendarBottomPadding +=
+                                getMonthHeight(calendar.max.weeks);
 
-                                calendarBottomPadding +=
-                                    getMonthHeight(calendar.max.weeks);
+                            calendar.max.remove();
+                        }
 
-                                calendar.max.remove();
-                            }
+                        scrollTop = calendarInitialPadding + scrollTop - calendarTopPadding;
+                        if (containerDom.scrollTop !== scrollTop)
+                            containerDom.scrollTop = scrollTop;
 
-                            element.css({
-                                'padding-top': calendarTopPadding + 'px',
-                                'padding-bottom': calendarBottomPadding + 'px'
-                            });
-
-                            scope.$$watchers = watchers;
-                        });
-
-                        $$rAF(load);
+                        scope.$$watchers = watchers;
                     };
 
-                    var resetId,
-                        reset = function () {
-                            $log.debug('antixCalendar.reset()');
+                    var resetId;
 
-                            var offset = containerDom.scrollTop - calendarTopPadding;
-                            calendarTopPadding = calendarInitialPadding;
-                            calendarBottomPadding = calendarInitialPadding;
-
-                            element.css({
-                                'padding-top': calendarTopPadding + 'px',
-                                'padding-bottom': calendarBottomPadding + 'px'
-                            });
-
-                            containerDom.scrollTop = calendarTopPadding + offset;
-                        };
-
-                    var start = function () {
-                        $$rAF(reset);
-                        $$rAF(load);
-                    };
-
-                    start();
-
-                    angular.element(document)
+                    angular.element(window)
                         .on('scroll', function () {
+
                             if (resetId) {
                                 $timeout.cancel(resetId);
                                 resetId = null;
                             }
 
                             resetId = $timeout(function () {
-                                $$rAF(reset);
-                            }, 500);
+                                scope.$evalAsync(reset);
+                            }, 150);
                         });
+
+                    scope.$evalAsync(reset);
+                    $timeout(function () {
+                        containerDom.scrollTop = calendarInitialPadding + 200;
+                    });
                 }
             }
         }
